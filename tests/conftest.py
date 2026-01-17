4c545cc0c5eaa074d7836ed5f1b056a80a63020e
Seyfullah Ismailov <seyfullahismailly@gmail.com>
2026-01-17 18:40:39 +0300

    -HomeWork ReDone

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,17 @@ from selene import browser
 
 @pytest.fixture(autouse=True)
 def browser_management():
+    browser.config.timeout = 10
     browser.driver.maximize_window()
     browser.open('https://demoqa.com/automation-practice-form')
+
+    browser.driver.execute_script("""
+    const fixedBan = document.getElementById('fixedban');
+    if (fixedBan) fixedBan.remove();
+
+    const footer = document.querySelector('footer');
+    if (footer) footer.remove();
+    """)
+
     yield
     browser.quit()
-
