54f9adcacc5f4fe4359e7fc08a899c87785ca6ec
Seyfullah Ismailov <seyfullahismailly@gmail.com>
2026-01-20 17:04:36 +0300

    -HomeWork ReDone

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,15 +6,5 @@ from selene import browser
 def browser_management():
     browser.config.timeout = 10
     browser.driver.maximize_window()
-    browser.open('https://demoqa.com/automation-practice-form')
-
-    browser.driver.execute_script("""
-    const fixedBan = document.getElementById('fixedban');
-    if (fixedBan) fixedBan.remove();
-
-    const footer = document.querySelector('footer');
-    if (footer) footer.remove();
-    """)
-
     yield
     browser.quit()
--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,21 +1,29 @@
 import os
-import time
 from selene import browser, have
 
 
+def remove_banners():
+    browser.driver.execute_script("""
+        const fixedBan = document.getElementById('fixedban');
+        if (fixedBan) fixedBan.remove();
+
+        const footer = document.querySelector('footer');
+        if (footer) footer.remove();
+    """)
+
+
 def test_registration_form():
+    browser.open('https://demoqa.com/automation-practice-form')
+    remove_banners()
+
     browser.element('#firstName').type('Seyfi')
     browser.element('#lastName').type('Ismailov')
-    time.sleep(1)
 
     browser.element('#userEmail').type('seyfiismailov@gmail.com')
-    time.sleep(1)
 
     browser.element('label[for="gender-radio-1"]').click()
-    time.sleep(1)
 
     browser.element('#userNumber').type('9219212121')
-    time.sleep(1)
 
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').click()
@@ -23,18 +31,15 @@ def test_registration_form():
     browser.element('.react-datepicker__year-select').click()
     browser.element('.react-datepicker__year-select option[value="1993"]').click()
     browser.element('[aria-label="Choose Saturday, August 21st, 1993"]').click()
-    time.sleep(1)
 
     browser.element('#subjectsInput').type('E')
     browser.element('#react-select-2-option-0').click()
     browser.element('#subjectsInput').type('A')
     browser.element('#react-select-2-option-2').click()
-    time.sleep(1)
 
     browser.element('label[for="hobbies-checkbox-1"]').click()
     browser.element('label[for="hobbies-checkbox-2"]').click()
     browser.element('label[for="hobbies-checkbox-3"]').click()
-    time.sleep(1)
 
     browser.element('#uploadPicture').send_keys(
         os.path.abspath(
@@ -44,22 +49,26 @@ def test_registration_form():
             )
         )
     )
-    time.sleep(1)
 
     browser.element('#currentAddress').type("Saint-Petersburg, Aptekarskaya street, 5")
-    time.sleep(1)
 
     browser.element('#state').click()
     browser.all('div[id^=react-select-3-option]').element_by(have.exact_text('NCR')).click()
     browser.element('#city').click()
     browser.all('div[id^="react-select-4-option"]').element_by(have.exact_text("Delhi")).click()
-    time.sleep(1)
 
     browser.element('#submit').click()
-    time.sleep(1)
 
-    browser.element('.table-responsive').should(have.text('Seyfi Ismailov'))
-    browser.element('.table-responsive').should(have.text('seyfiismailov@gmail.com'))
+    results = browser.element('.table-responsive')
+    results.should(have.text('Seyfi Ismailov'))
+    results.should(have.text('seyfiismailov@gmail.com'))
+    results.should(have.text('Male'))
+    results.should(have.text('9219212121'))
+    results.should(have.text('21 August,1993'))
+    results.should(have.text('English, Arts'))
+    results.should(have.text('Sports, Reading, Music'))
+    results.should(have.text('test.png'))
+    results.should(have.text('Saint-Petersburg, Aptekarskaya street, 5'))
+    results.should(have.text('NCR Delhi'))
 
     browser.element('#closeLargeModal').click()
-    time.sleep(1)
